be51e12573ab81bb9de887e566f5a7f76cffb8ae
Vikrant Kekan <48706063+vikrantkekan@users.noreply.github.com>
2020-04-05 15:09:44 +0530

    Updated and Tested Verify Checksum
    there was some Error in code which is updated and tested for Paytm App integration.

--- verifyChecksum.php
+++ verifyChecksum.php
@@ -13,40 +13,34 @@ $isValidChecksum = FALSE;
 
 $paramList = $_POST;
 $return_array = $_POST;
+$return_array["IS_CHECKSUM_VALID"] = "N"; // by default set IS_CHECKSUM_VALID is N
 $paytmChecksum = isset($_POST["CHECKSUMHASH"]) ? $_POST["CHECKSUMHASH"] : ""; //Sent by Paytm pg
 
 //Verify all parameters received from Paytm pg to your application. Like MID received from paytm pg is same as your application’s MID, TXN_AMOUNT and ORDER_ID are same as what was sent by you to Paytm PG for initiating transaction etc.
 $isValidChecksum = verifychecksum_e($paramList, PAYTM_MERCHANT_KEY, $paytmChecksum); //will return TRUE or FALSE string.
 
-// if ($isValidChecksum===TRUE)
-// 	$return_array["IS_CHECKSUM_VALID"] = "Y";
-// else
-// 	$return_array["IS_CHECKSUM_VALID"] = "N";
+ if($isValidChecksum==='TRUE'){ // change by vikrant k ($isValidChecksum not returning boolean its returns string{TRUE/FALSE})
+ 	$return_array["IS_CHECKSUM_VALID"] = "Y";
+ }
+ else{	
+     $return_array["IS_CHECKSUM_VALID"] = "N";
+     }
 
-$return_array["IS_CHECKSUM_VALID"] = $isValidChecksum ? "Y" : "N";
+
+//$return_array["IS_CHECKSUM_VALID"] = $isValidChecksum ? "Y" : "N";
 //$return_array["TXNTYPE"] = "";
 //$return_array["REFUNDAMT"] = "";
+/*
 unset($return_array["CHECKSUMHASH"]);
-
 $encoded_json = htmlentities(json_encode($return_array));
+*/
+
+//print_r($isValidChecksum);
+//added change by vikrant k(return reposnse in json)
+header('Content-type: application/json');
+echo json_encode($return_array); 
+
 
 ?>
 
-<html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html;charset=ISO-8859-I">
-	<title>Paytm</title>
-	<script type="text/javascript">
-		function response(){
-			return document.getElementById('response').value;
-		}
-	</script>
-</head>
-<body>
-  Redirect back to the app<br>
 
-  <form name="frm" method="post">
-    <input type="hidden" id="response" name="responseField" value='<?php echo $encoded_json?>'>
-  </form>
-</body>
-</html>
